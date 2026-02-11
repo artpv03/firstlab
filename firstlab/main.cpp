653db8665b469f43407d5ed40f84d23a6d3803c8
Artur <artur40507@gmail.com>
2026-02-11 22:32:21 +0500

    все готово

--- firstlab/main.cpp
+++ firstlab/main.cpp
@@ -1,7 +1,8 @@
 ﻿
-
+#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cmath>
+#include <stdio.h>
 using namespace std;
 int fib() {
     unsigned short MAX;
@@ -32,11 +33,43 @@ int fib() {
 }
 int func() {
     float ans = 0.0, x = 1, y = 4, step = (y - x) / 9;
-    for (int i = 0; i <= 9; i++) {
+    for (int i = 1; i <= 9; i++) {
         ans = sin(x) / x;
         cout << ans << endl;
         x += step;
     }
+    return 0;
+}
+int table() {
+    double cred, perc;
+    int mon;
+
+    printf("Введите сумму кредита: ");
+    scanf("%lf", &cred);
+
+    printf("Введите срок кредита (в месяцах): ");
+    scanf("%d", &mon);
+
+    printf("Введите процентную ставку: ");
+    scanf("%lf", &perc);
+
+    double monthrate = perc / 12 / 100;
+    double mainpay = cred / mon;
+    double balance = cred;
+    double totalperc = 0;
+
+    printf("\n№   Долг        Процент    Платеж\n");
+
+    for (int i = 1; i <= mon; i++) {
+        double interest = balance * monthrate;
+        double pay = mainpay + interest;
+        printf("%-3d %-12.2lf %-12.2lf %-12.2lf\n", i, balance, interest, pay);
+        totalperc += interest;
+        balance -= mainpay;
+    }
+
+    printf("\nОбщий процент: %.2lf\n", totalperc);
+
     return 0;
 }
 int main() {
@@ -46,5 +79,6 @@ int main() {
     cin >> nom;
     if (nom == 1) fib();
     if (nom == 2) func();
+    if (nom == 3) table();
     return 0;
 }
