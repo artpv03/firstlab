0b6dede5b05b2c02764d3fa19962a827360a9336
Artur <artur40507@gmail.com>
2026-02-11 16:03:34 +0500

    first

--- firstlab/firstlab.cpp
+++ firstlab/firstlab.cpp
@@ -1,8 +1,27 @@
 ﻿
 
 #include <iostream>
-
-int main()
-{
-    std::cout << "Hello World!\n";
+using namespace std;
+int main(){
+    setlocale(LC_ALL, "RU");
+    short MAX, a = 0, b = 1;
+    cout << "Введите n-ый член:";
+    cin >> MAX;
+    if (MAX == 0) {
+        cout << a << endl;
+        return 0;
+    }
+    if (MAX == 1) {
+        cout << b << endl;
+        return 0;
+    }
+    for (short i=2; i<=MAX; i++) {
+        short next = a + b;
+        a = b;
+        b = next;
+    }
+    cout << b << endl;
+    return 0;
+        
+    
 }
