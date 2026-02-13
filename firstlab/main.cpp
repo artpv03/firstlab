ddf23d9582cfa449f74abdde59f6053b5b3d5164
Artur <artur40507@gmail.com>
2026-02-13 22:24:03 +0500

    изменения задания с числами Фибоначчи, добавление ввода начального значения в задании с ф-цией

--- firstlab/main.cpp
+++ firstlab/main.cpp
@@ -1,39 +1,32 @@
-﻿
-#define _CRT_SECURE_NO_WARNINGS
+﻿#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <cmath>
 #include <stdio.h>
 using namespace std;
 int fib() {
-    unsigned short MAX;
-    long sum = 0, a = 0, b = 1;
-    cout << "Введите n-ый член:";
+    long long MAX;
+    long long a = 0, b = 1, sum = a + b, n=2;
+    cout << "Введите максимальное число:";
     cin >> MAX;
-    if (MAX == 1) {
-        cout << "Число: " << a << endl;
-        cout << "Сумма: " << a << endl;
-        return 0;
-    }
-    if (MAX == 2) {
-        cout << "Число: " << b << endl;
-        cout << "Cумма: " << b << endl;
-        return 0;
-    }
-    sum = a + b;
-    for (unsigned short i = 3; i <= MAX; i++) {
-        long next = a + b;
+    while(sum<=MAX) {
+        long long next = a + b;
         sum += next;
+        n++;
         a = b;
         b = next;
     }
-    cout << "Число: " << b << endl;
+    cout << "Количество чисел: " << n << endl;
     cout << "Сумма: " << sum << endl;
 
     return 0;
 }
 int func() {
-    float ans = 0.0, x = 1, y = 4, step = (y - x) / 9;
-    for (int i = 1; i <= 9; i++) {
+    float x;
+    float ans = 0.0, y = 4; 
+    cout << "Введите начальное значение интервала:";
+    cin >> x;
+    float  step = (y - x) / 9;
+    for (int i = 0; i <= 9; i++) {
         ans = sin(x) / x;
         cout << ans << endl;
         x += step;
