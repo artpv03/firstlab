c0adb1d4ba007639ff8a0778ce9639d6c1859c30
Artur <artur40507@gmail.com>
2026-02-27 00:38:47 +0500

    задание 4 через класс стринг

--- laba9 stroki.cpp
+++ laba9 stroki.cpp
@@ -2,11 +2,10 @@
 #include <cstring>
 using namespace std;
 int massstrC(){
-    setlocale(LC_ALL, "RU");
     int len;
     cout<<"Введите размер массива: "<<endl;
     cin>>len;
-    if (len<0 || len>20){
+    if (len<=0 || len>20){
         cout<<"Неверный размер";
         return 0;
     }
@@ -47,10 +46,42 @@ int massstrC(){
     delete[] str;
 return 0;
 }
+int massstrCPP(){
+    int len;
+    cout<<"Введите размер массива: "<<endl;
+    cin>>len;
+    if (len<=0 || len>20){
+        cout<<"Неверный размер";
+        return 0;
+    }
+cout<<"---------------------------------------"<<endl;
+    string str[20];
+    cout<<"Введите слова для массива: "<<endl;
+    for(int i=0; i<len; i++){
+        cin>>str[i];
+        if(str[i].length()>10){
+            cout<<"Слово длиннее 10 символов!"<<endl;
+            return 0;
+        }
+    }
+    cout<<"---------------------------------------"<<endl;
+    cout<<"Вывод слов только с четным номерами: "<<endl;
+    if(len==1){
+        cout<<"Нет слов с четным номером"<<endl;
+    }
+    else{
+        for(int i=1; i<len; i+=2){
+            cout<<str[i]<<endl;
+        }
+    }
+    cout<<"---------------------------------------"<<endl;
+    return 0;
+}
 int main(){
 setlocale(LC_ALL, "RU");
     short nom;
     cout << "Выберите задание(1-2):";
     cin >> nom;
     if (nom == 1) massstrC();
+    else if (nom == 2) massstrCPP();
 }
