2562ebe9720cf33ee64310fa91f4e4cbac61b9dc
Artur <artur40507@gmail.com>
2026-02-27 00:01:07 +0500

    задание 4 через массив символов

---
+++ laba9 stroki.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+int massstrC(){
+    setlocale(LC_ALL, "RU");
+    int len;
+    cout<<"Введите размер массива: "<<endl;
+    cin>>len;
+    if (len<0 || len>20){
+        cout<<"Неверный размер";
+        return 0;
+    }
+    cout<<"---------------------------------------"<<endl;
+    char **str;
+    str=new char*[len];
+    for(int i=0; i<len; i++){
+        str[i] = new char [20];
+    }
+    cout<<"Введите слова для массива: "<<endl;
+    for(int i=0; i<len; i++){
+        cin>>str[i];
+        if(strlen(str[i])>20){
+            cout<<"Слово длиннее 10 символов!"<<endl;
+            for(int j=0; j<len; j++)
+                delete[] str[j];
+            delete[] str;
+            return 0;
+        }
+    }
+    cout<<"---------------------------------------"<<endl;
+    cout<<"Введённые слова для массива: "<<endl;
+    for(int i=0; i<len; i++){
+        cout<<str[i]<<endl;
+    }
+    cout<<"---------------------------------------"<<endl;
+    cout<<"Вывод слов только с четным номерами: "<<endl;
+    if(len==1){
+        cout<<"Нет слов с четным номером"<<endl;
+    }
+    else{
+        for(int i=1; i<len; i+=2)
+        cout<<str[i]<<endl;
+    }
+    cout<<"---------------------------------------"<<endl;
+    for(int i=0; i<len; i++)
+        delete[] str[i];
+    delete[] str;
+return 0;
+}
+int main(){
+setlocale(LC_ALL, "RU");
+    short nom;
+    cout << "Выберите задание(1-2):";
+    cin >> nom;
+    if (nom == 1) massstrC();
+}
